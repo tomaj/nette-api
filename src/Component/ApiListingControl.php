54781ab995ae1f6ea4b09632567c71e26c79e34c
Tomas Majer <tomasmajer@gmail.com>
2015-12-25 21:27:09 +0100

    Added endpoint listing component

---
+++ src/Component/ApiListingControl.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tomaj\NetteApi\Component;
+
+use Nette\Application\UI\Control;
+use Nette\ComponentModel\IContainer;
+use Tomaj\NetteApi\ApiDecider;
+use Closure;
+use Exception;
+
+class ApiListingControl extends Control
+{
+    /**
+     * @var ApiDecider
+     */
+    private $apiDecider;
+
+    private $clickCallback;
+
+    public function __construct(IContainer $parent, $name, ApiDecider $apiDecider)
+    {
+        parent::__construct(null, $name);
+        $this->apiDecider = $apiDecider;
+    }
+
+
+    public function onClick(Closure $callback)
+    {
+        $this->clickCallback = $callback;
+    }
+
+    public function render()
+    {
+        $handlers = $this->apiDecider->getHandlers();
+        $this->template->handlers = $this->sortHandlers($handlers);
+        $this->template->setFile(__DIR__ . '/api_listing.latte');
+        $this->template->render();
+    }
+
+    public function handleSelect($method, $version, $package, $apiAction)
+    {
+        if (!$this->clickCallback) {
+            throw new Exception('You have to set onClick callback to component!');
+        }
+
+        $this->clickCallback->__invoke($method, $version, $package, $apiAction);
+    }
+
+    private function sortHandlers($handlers)
+    {
+        $versionHandlers = [];
+        foreach ($handlers as $handler) {
+            $endPoint = $handler['endpoint'];
+            if (!isset($versionHandlers[$endPoint->getVersion()])) {
+                $versionHandlers[$endPoint->getVersion()] = [];
+            }
+            $versionHandlers[$endPoint->getVersion()][] = $handler;
+        }
+        return $versionHandlers;
+    }
+}
