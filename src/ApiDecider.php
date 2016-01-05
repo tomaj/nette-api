5fd6e4af4c83064f15337578b3b8111c22e65075
Tomas Majer <tomasmajer@gmail.com>
2016-01-05 22:57:18 +0100

    Removed unused comment

--- src/ApiDecider.php
+++ src/ApiDecider.php
@@ -8,8 +8,6 @@ use Tomaj\NetteApi\Handlers\ApiHandlerInterface;
 use Tomaj\NetteApi\Handlers\DefaultHandler;
 use Tomaj\NetteApi\Link\ApiLink;
 
-// TODO - refactor - remove array with triplet, replace with some class
-
 class ApiDecider
 {
     /**
@@ -88,48 +86,4 @@ class ApiDecider
     {
         return $this->handlers;
     }
-
-    /*
-    public function getHandlersList($version)
-    {
-        $list = [];
-        foreach ($this->getHandlers() as $handler) {
-            $endpoint = $handler['endpoint'];
-            if ($version && $version != $endpoint->getVersion()) {
-                continue;
-            }
-            $item = [
-                'method' => $endpoint->getMethod(),
-                'version' => $endpoint->getVersion(),
-                'package' => $endpoint->getPackage(),
-                'api_action' => $endpoint->getApiAction(),
-                'url' => $this->apiLink->link($endpoint),
-            ];
-            $params = $this->createParamsList($handler);
-            if (count($params) > 0) {
-                $item['params'] = $params;
-            }
-            $list[] = $item;
-        }
-        return $list;
-    }
-    
-    private function createParamsList($handler)
-    {
-        $paramsList = $handler['handler']->params();
-        $params = [];
-        foreach ($paramsList as $param) {
-            $parameter = [
-                'type' => $param->getType(),
-                'key' => $param->getKey(),
-                'is_required' => $param->isRequired(),
-            ];
-            if ($param->getAvailableValues()) {
-                $parameter['available_values'] = $param->getAvailableValues();
-            }
-            $params[] = $parameter;
-        }
-        return $params;
-    }
-    */
 }
