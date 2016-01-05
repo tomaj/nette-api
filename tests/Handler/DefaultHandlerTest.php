16a5f6d7178ab78365865bd97b3a5a3d0acd076b
Tomas Majer <tomasmajer@gmail.com>
2016-01-05 13:03:41 +0100

    Added default handler test

---
+++ tests/Handler/DefaultHandlerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tomaj\NetteApi\Test\Handler;
+
+use PHPUnit_Framework_TestCase;
+use Tomaj\NetteApi\Handlers\DefaultHandler;
+
+class DefaultHandlerTest extends PHPUnit_Framework_TestCase
+{
+	public function testResponse()
+	{
+		$defaultHandler = new DefaultHandler();
+		$result = $defaultHandler->handle([]);
+		$this->assertEquals(500, $result->getCode());
+		$this->assertEquals('application/json; charset=utf-8', $result->getContentType());
+		$this->assertEquals(['status' => 'error', 'message' => 'Unknown api endpoint'], $result->getPayload());
+	}
+}
