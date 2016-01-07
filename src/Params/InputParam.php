2cfb2ffb5e352c0814152d853a4b0450c52d24d9
Tomas Majer <tomasmajer@gmail.com>
2016-01-07 10:27:49 +0100

    Fixed support for array inputs in GET and POST

--- src/Params/InputParam.php
+++ src/Params/InputParam.php
@@ -13,14 +13,29 @@ class InputParam implements ParamInterface
     const OPTIONAL = false;
     const REQUIRED = true;
 
+    /**
+     * @var string
+     */
     private $type;
 
+    /**
+     * @var string
+     */
     private $key;
 
+    /**
+     * @var bool
+     */
     private $required;
 
+    /**
+     * @var array|null
+     */
     private $availableValues;
 
+    /**
+     * @var bool
+     */
     private $multi;
 
     public function __construct($type, $key, $required = self::OPTIONAL, $availableValues = null, $multi = false)
@@ -69,6 +84,13 @@ class InputParam implements ParamInterface
         return $this->multi;
     }
 
+    /**
+     * Check if actual value from environment is valid
+     *
+     * @return bool
+     *
+     * @throws Exception if actual InputParam has unsupported type
+     */
     public function isValid()
     {
         $value = $this->getValue();
@@ -89,17 +111,30 @@ class InputParam implements ParamInterface
         return true;
     }
 
+    /**
+     * Process environment variables like POST|GET|etc.. and return actual value
+     *
+     * @return mixed
+     *
+     * @throws Exception if actual InputParam has unsupported type
+     */
     public function getValue()
     {
         if ($this->type == self::TYPE_GET) {
             if (!filter_has_var(INPUT_GET, $this->key) && isset($_GET[$this->key])) {
                 return $_GET[$this->key];
             }
+            if ($this->isMulti()) {
+                return filter_input(INPUT_GET, $this->key, FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+            }
             return filter_input(INPUT_GET, $this->key);
         }
         if ($this->type == self::TYPE_POST) {
             if (!filter_has_var(INPUT_POST, $this->key) && isset($_POST[$this->key])) {
                 return $_POST[$this->key];
+            }
+            if ($this->isMulti()) {
+                return filter_input(INPUT_POST, $this->key, FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
             }
             return filter_input(INPUT_POST, $this->key);
         }
@@ -108,7 +143,7 @@ class InputParam implements ParamInterface
                 return $_FILES[$this->key];
             }
         }
-        
+
         throw new Exception("Invalid type");
     }
 }
