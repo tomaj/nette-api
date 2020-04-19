088e2e948c5239b05bb64c2d35c6b79d96302522
Michal Lulco <lulco@efabrica.sk>
2020-04-20 00:16:58 +0200

    Removed debug output

--- src/RateLimit/NoRateLimit.php
+++ src/RateLimit/NoRateLimit.php
@@ -2,15 +2,10 @@
 
 namespace Tomaj\NetteApi\RateLimit;
 
-use Nette\Application\Responses\TextResponse;
-
 class NoRateLimit implements RateLimitInterface
 {
     public function check(): ?RateLimitResponse
     {
-
-        return new RateLimitResponse(40, 0, 123, new TextResponse('blablabla'));
-
         return null;
     }
 }
