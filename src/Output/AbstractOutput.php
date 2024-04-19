5347a7129679af7027a7f98a16ab583b57297afe
Martin Beránek <m.beranek111@gmail.com>
2024-04-19 20:44:13 +0200

    Refactor JsonOutput and OpenApiHandler classes to support multiple examples (#143)
    Co-authored-by: Martin Beranek <martin.beranek@cloudtalk.io>

--- src/Output/AbstractOutput.php
+++ src/Output/AbstractOutput.php
@@ -10,6 +10,9 @@ abstract class AbstractOutput implements OutputInterface
 
     protected $description;
 
+    /** @var array */
+    protected $examples = [];
+
     public function __construct(int $code, string $description = '')
     {
         $this->code = $code;
@@ -24,5 +27,49 @@ abstract class AbstractOutput implements OutputInterface
     public function getDescription(): string
     {
         return $this->description;
+    }
+
+    /**
+     * @param string $name Example name
+     * @param mixed $example Example
+     * @return Self
+     */
+    public function addExample(string $name, $example): self
+    {
+        $this->examples[$name] = $example;
+        return $this;
+    }
+
+    /**
+     * Set default example
+     * @param mixed $example
+     * @return self
+     * @deprecated Use addExample instead
+     */
+    public function setExample($example): self
+    {
+        $this->examples["default"] = $example;
+        return $this;
+    }
+
+    /**
+     * Returns first example
+     * @return mixed
+     */
+    public function getExample()
+    {
+        if (empty($this->examples)) {
+            return null;
+        }
+        return reset($this->examples);
+    }
+
+    /**
+     * Returns all examples
+     * @return array
+     */
+    public function getExamples(): array
+    {
+        return $this->examples;
     }
 }
