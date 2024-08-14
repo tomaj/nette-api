a3899770095c8a40e1ea9accbe161d60e5eeeae6
Martin Beranek <m.beranek111@gmail.com>
2024-08-14 11:47:53 +0200

    old php support

--- src/Output/Configurator/QueryConfigurator.php
+++ src/Output/Configurator/QueryConfigurator.php
@@ -10,16 +10,18 @@ class QueryConfigurator implements ConfiguratorInterface
 {
     private $schemaValidateParam = 'schema_validate';
     private $errorDetailParam = 'error_detail';
+    public $request = null;
 
     /**
      * @param string $schemaValidateParam Name of get parameter to enable schema validation
      * @param string $errorDetailParam Name of get parameter to show additional info in error response
      */
     public function __construct(
-        public readonly Request $request,
+        Request $request,
         string $schemaValidateParam = 'schema_validate', 
         string $errorDetailParam = 'error_detail'
     ) {
+        $this->request = $request;
         $this->schemaValidateParam = $schemaValidateParam;
         $this->errorDetailParam = $errorDetailParam;
     }
