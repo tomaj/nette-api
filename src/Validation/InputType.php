86f902adf1bb8ffd131349c8541271763a369adf
Martin Beránek <m.beranek111@gmail.com>
2026-01-04 12:58:05 +0100

    Validate all input types (#159)

---
+++ src/Validation/InputType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tomaj\NetteApi\Validation;
+
+class InputType
+{
+    public const BOOLEAN = 'boolean';
+    public const INTEGER = 'integer';
+    public const DOUBLE = 'double';
+    public const FLOAT = 'float';
+    public const STRING = 'string';
+    public const ARRAY = 'array';
+}
